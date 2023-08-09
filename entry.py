b4852a5c0ac0969e6a89cb22bcf0346e75ddc738
lvmin <lyuminzhang@outlook.com>
2023-08-09 12:55:33 -0700

    i

--- entry.py
+++ entry.py
@@ -1,5 +1,6 @@
 import os
 import torch
+import safetensors.torch
 
 from omegaconf import OmegaConf
 from sgm.util import instantiate_from_config
@@ -7,5 +8,9 @@ from sgm.util import instantiate_from_config
 config_path = './sd_xl_base.yaml'
 config = OmegaConf.load(config_path)
 model = instantiate_from_config(config.model).cpu()
+model.eval()
+
+sd = safetensors.torch.load_file('./sd_xl_base_1.0.safetensors')
+model.load_state_dict(sd, strict=False)
 
 a = 0
