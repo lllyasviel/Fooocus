60682c2f77880a44468cf491ac3a8a9dda0c93e7
lvmin <lyuminzhang@outlook.com>
2023-08-10 05:10:13 -0700

    i

--- webui.py
+++ webui.py
@@ -15,5 +15,7 @@ from modules.path import modelfile_path
 xl_base_filename = os.path.join(modelfile_path, 'sd_xl_base_1.0.safetensors')
 xl_refiner_filename = os.path.join(modelfile_path, 'sd_xl_refiner_1.0.safetensors')
 
-xl_base, xl_base_clip, xl_base_vae = load_checkpoint_guess_config(xl_base_filename)
+xl_base, xl_base_clip, xl_base_vae, xl_base_clipvision = load_checkpoint_guess_config(xl_base_filename)
+del xl_base_clipvision
+
 a = 0
