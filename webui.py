f91af9cbddd6255132d74ed1d5af26b72fabf513
lvmin <lyuminzhang@outlook.com>
2023-08-10 07:07:17 -0700

    i

--- webui.py
+++ webui.py
@@ -8,12 +8,10 @@ def generate_clicked(positive_prompt):
 
 block = gr.Blocks().queue()
 with block:
-    with gr.Row():
-        with gr.Column():
-            prompt = gr.Textbox(label="Prompt", value='a handsome man in forest')
-            run_button = gr.Button(label="Run")
-        with gr.Column():
-            result_gallery = gr.Gallery(label='Output', show_label=False, elem_id="gallery").style(grid=2, height='auto')
+    with gr.Column():
+        prompt = gr.Textbox(label="Prompt", value='a handsome man in forest')
+        run_button = gr.Button(label="Run")
+        result_gallery = gr.Gallery(label='Output', show_label=False, elem_id="gallery", height=700)
     run_button.click(fn=generate_clicked, inputs=[prompt], outputs=[result_gallery])
 
 
