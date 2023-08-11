aa4396e67018a6d6a5282a4d2d68af1da895b68d
lvmin <lyuminzhang@outlook.com>
2023-08-10 19:00:23 -0700

    i

--- draft.py
+++ draft.py
@@ -20,7 +20,7 @@ with block:
     with gr.Column():
         prompt = gr.Textbox(label="Prompt", value='a handsome man in forest')
         run_button = gr.Button(label="Run")
-        result_gallery = gr.Gallery(label='Output', show_label=False, elem_id="gallery", height=1280)
+        result_gallery = gr.Gallery(label='Output', show_label=False, elem_id="gallery", height='auto')
     run_button.click(fn=generate_clicked, inputs=[prompt], outputs=[result_gallery])
 
 
