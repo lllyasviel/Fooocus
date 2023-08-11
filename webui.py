4421506b676634529241ac66832d4186aa196da0
lvmin <lyuminzhang@outlook.com>
2023-08-10 20:14:30 -0700

    i

--- webui.py
+++ webui.py
@@ -15,17 +15,21 @@ def generate_clicked(positive_prompt):
                    negative_prompt=n)
 
 
-block = gr.Blocks().queue()
+block = gr.Blocks()
 with block:
-    with gr.Column():
-        gallery = gr.Gallery(label='Gallery', show_label=False, object_fit='contain', height=768)
-        with gr.Row():
-            with gr.Column(scale=0.85):
-                prompt = gr.Textbox(show_label=False, placeholder="Type prompt here.", container=False)
-            with gr.Column(scale=0.15, min_width=0):
-                run_button = gr.Button(label="Generate", value="Generate")
-        with gr.Row():
-            advanced_checkbox = gr.Checkbox(label='Advanced', value=False, container=False)
-    run_button.click(fn=generate_clicked, inputs=[prompt], outputs=[gallery])
+    with gr.Row():
+        with gr.Column(scale=0.7):
+            gallery = gr.Gallery(label='Gallery', show_label=False, object_fit='contain', height=768)
+            with gr.Row():
+                with gr.Column(scale=0.85):
+                    prompt = gr.Textbox(show_label=False, placeholder="Type prompt here.", container=False)
+                with gr.Column(scale=0.15, min_width=0):
+                    run_button = gr.Button(label="Generate", value="Generate")
+            with gr.Row():
+                advanced_checkbox = gr.Checkbox(label='Advanced', value=False, container=False)
+        with gr.Column(scale=0.3):
+            with gr.Group():
+                gr.Textbox()
+        run_button.click(fn=generate_clicked, inputs=[prompt], outputs=[gallery])
 
 block.launch()
