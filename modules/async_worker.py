8c64c6ceb9cd4015296b302c9d95f9c36cc7b10b
lvmin <lyuminzhang@outlook.com>
2023-08-12 23:27:00 -0700

    i

--- modules/async_worker.py
+++ modules/async_worker.py
@@ -27,6 +27,7 @@ def worker():
         pipeline.refresh_base_model(base_model_name)
         pipeline.refresh_refiner_model(refiner_model_name)
         pipeline.refresh_loras(loras)
+        pipeline.clean_prompt_cond_caches()
 
         p_txt, n_txt = apply_style(style_selction, prompt, negative_prompt)
 
