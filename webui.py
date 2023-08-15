387bef111643b6f2421c430c0c0945c90977e294
lvmin <lyuminzhang@outlook.com>
2023-08-15 10:47:11 -0700

    i

--- webui.py
+++ webui.py
@@ -79,6 +79,7 @@ with shared.gradio_root:
                     model_refresh = gr.Button(label='Refresh', value='\U0001f504 Refresh All Files', variant='secondary', elem_classes='refresh_button')
                 with gr.Accordion(label='Advanced', open=False):
                     sharpness = gr.Slider(label='Sampling Sharpness', minimum=0.0, maximum=40.0, step=0.01, value=2.0)
+                    gr.HTML('<a href="https://github.com/lllyasviel/Fooocus/discussions/117">\U0001F4D4 Document</a>')
 
                 def model_refresh_clicked():
                     modules.path.update_all_model_names()
