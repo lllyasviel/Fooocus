5d179e1eaac91906fe34a90baa1c31cdb2018b4c
lvmin <lyuminzhang@outlook.com>
2023-09-14 18:56:01 -0700

    fix vm

--- modules/virtual_memory.py
+++ modules/virtual_memory.py
@@ -60,7 +60,7 @@ def move_to_virtual_memory(model, comfy_unload=True):
     virtual_memory_device_dict = {k: torch.device(index=v.device.index, type=v.device.type) for k, v in sd.items()}
     virtual_memory_filename = os.path.join(virtual_memory_path, model_hash)
     if not os.path.exists(virtual_memory_filename):
-        print(f'[Virtual Memory System] Creating virtual memory environments ... This may be slow in the first time ...')
+        print(f'[Virtual Memory System] Creating virtual memory environments ... This may take at most 20 seconds in the first time ...')
         print(f'If the website application crashes during this process, please refresh the webpage ...')
         if use_safetensors:
             sf.save_file(sd, virtual_memory_filename)
