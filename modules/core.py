7b5e02e11c84546408cae61182b86abe065d5992
lvmin <lyuminzhang@outlook.com>
2023-09-14 17:39:57 -0700

    fix vm

--- modules/core.py
+++ modules/core.py
@@ -18,10 +18,10 @@ from modules.patch import patch_all
 patch_all()
 opEmptyLatentImage = EmptyLatentImage()
 opVAEDecode = VAEDecode()
-sha256_hash = hashlib.sha256()
 
 
 def sha256_hash_string(input_string):
+    sha256_hash = hashlib.sha256()
     sha256_hash.update(input_string.encode('utf-8'))
     return sha256_hash.hexdigest()[:32]
 
