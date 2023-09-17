789b1fce13dbf4cee92edc56ebe5a06a5c927f01
lvmin <lyuminzhang@outlook.com>
2023-09-16 20:02:55 -0700

    i

--- modules/inpaint_worker.py
+++ modules/inpaint_worker.py
@@ -7,6 +7,6 @@ class InpaintWorker:
         self.raw_mask = mask
         # Fooocus inpaint logic
         # 1. ensure that diffusion area cover all masks.
-        # 2. ensure that diffusion area cover at lease 30% part of images.
-        # 3. ensure that diffusion area has at least 1k resolution.
+        # 2. ensure that diffusion area cover at lease 1/4 part of images.
+        # 3. ensure that diffusion area has at least 1k resolution (by resampling).
         raise NotImplemented
