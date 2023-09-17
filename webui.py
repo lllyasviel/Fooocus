74b26a3cbb71c73deb6fef639179ab17dc5f8119
lllyasviel <lyuminzhang@outlook.com>
2023-09-17 02:00:26 -0700

    i (#401)

--- webui.py
+++ webui.py
@@ -66,7 +66,8 @@ with shared.gradio_root:
                         uov_input_image = gr.Image(label='Drag above image to here', source='upload', type='numpy')
                         uov_method = gr.Radio(label='Method', choices=flags.uov_list, value=flags.disabled, show_label=False, container=False)
                     gr.HTML('<a href="https://github.com/lllyasviel/Fooocus/discussions/390">\U0001F4D4 Document</a>')
-            input_image_checkbox.change(lambda x: gr.update(visible=x), inputs=input_image_checkbox, outputs=image_input_panel, queue=False)
+            input_image_checkbox.change(lambda x: gr.update(visible=x), inputs=input_image_checkbox, outputs=image_input_panel, queue=False,
+                                        _js="(x) => {if(x){setTimeout(() => window.scrollTo({ top: window.scrollY + 500, behavior: 'smooth' }), 50);}else{setTimeout(() => window.scrollTo({ top: 0, behavior: 'smooth' }), 50);} return x}")
 
             # def get_select_index(g, evt: gr.SelectData):
             #     return g[evt.index]['name']
