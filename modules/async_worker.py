7aec15e753a696dc5f81beb46300c56ffc87b4fb
lvmin <lyuminzhang@outlook.com>
2023-09-17 23:17:39 -0700

    i

--- modules/async_worker.py
+++ modules/async_worker.py
@@ -191,9 +191,9 @@ def worker():
                     inpaint_worker.current_task = inpaint_worker.InpaintWorker(image=inpaint_image, mask=inpaint_mask,
                                                                                is_outpaint=len(outpaint_selections) > 0)
 
-                    print(f'Inpaint task: {str((height, width))}')
-                    outputs.append(['results', inpaint_worker.current_task.visualize_mask_processing()])
-                    return
+                    # print(f'Inpaint task: {str((height, width))}')
+                    # outputs.append(['results', inpaint_worker.current_task.visualize_mask_processing()])
+                    # return
 
                     inpaint_pixels = core.numpy_to_pytorch(inpaint_worker.current_task.image_ready)
                     progressbar(0, 'VAE encoding ...')
@@ -205,6 +205,9 @@ def worker():
                     width = W * 8
                     height = H * 8
                     inpaint_worker.current_task.load_latent(latent=inpaint_latent, mask=inpaint_mask)
+
+                    initial_latent = inpaint_latent
+                    denoising_strength = 1.0
 
         progressbar(1, 'Initializing ...')
 
