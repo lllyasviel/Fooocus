eeb422772d4243de611849dc2c98e449f66172d9
StokeMcToke <stoke@stoketech.club>
2023-09-18 04:50:41 +0100

    Update launch.py
    updated with stoked reality stored on huggingface to see if any faster to download

--- launch.py
+++ launch.py
@@ -69,9 +69,16 @@ def prepare_environment():
     return
 
 
+#model_filenames = [
+#    ('sd_xl_base_1.0_0.9vae.safetensors',
+#     'https://civitai.com/api/download/models/162531?type=Model&format=SafeTensor&size=pruned&fp=fp16'),
+#    ('sd_xl_refiner_1.0_0.9vae.safetensors',
+#     'https://huggingface.co/stabilityai/stable-diffusion-xl-refiner-1.0/resolve/main/sd_xl_refiner_1.0_0.9vae.safetensors')
+#]
+
 model_filenames = [
     ('sd_xl_base_1.0_0.9vae.safetensors',
-     'https://civitai.com/api/download/models/162531?type=Model&format=SafeTensor&size=pruned&fp=fp16'),
+     'https://huggingface.co/StokeMcToke/StokedRealityXL/resolve/main/StokedRealityXL-v10_fp16.safetensors'),
     ('sd_xl_refiner_1.0_0.9vae.safetensors',
      'https://huggingface.co/stabilityai/stable-diffusion-xl-refiner-1.0/resolve/main/sd_xl_refiner_1.0_0.9vae.safetensors')
 ]
