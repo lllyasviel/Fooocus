41dde65f4e735b360f39f7f58adcff42c3a12db6
lvmin <lyuminzhang@outlook.com>
2023-09-19 03:25:01 -0700

    i

--- modules/inpaint_worker.py
+++ modules/inpaint_worker.py
@@ -198,7 +198,6 @@ class InpaintWorker:
         return y.clip(0, 255).astype(np.uint8)
 
     def post_process(self, img):
-        return img
         a, b, c, d = self.interested_area
         content = resample_image(img, d - c, b - a)
         result = self.image_raw.copy()
