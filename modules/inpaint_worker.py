93ee4eabe3c16fc6aad1e7b3c7159190451fab76
lllyasviel <lyuminzhang@outlook.com>
2023-09-21 03:04:27 -0700

    fix loop (#465)

--- modules/inpaint_worker.py
+++ modules/inpaint_worker.py
@@ -98,11 +98,15 @@ def solve_abcd(x, a, b, c, d, k, outpaint):
         if area_abcd(a, b, c, d) >= max_area:
             break
         if (b - a) < (d - c):
-            a -= 1
-            b += 1
-        else:
+            a -= 2
+            b += 2
             c -= 1
             d += 1
+        else:
+            a -= 1
+            b += 1
+            c -= 2
+            d += 2
         a, b, c, d = regulate_abcd(x, a, b, c, d)
     return a, b, c, d
 
