8b98a4b4c2d187e2ec5090f6792d3ee93555577d
lvmin <lyuminzhang@outlook.com>
2023-10-03 01:34:53 -0700

    i

--- modules/patch.py
+++ modules/patch.py
@@ -177,13 +177,12 @@ def patched_sampler_cfg_function(args):
     uncond = args['uncond'] * cfg_s + cfg_x0
     cond_scale = args['cond_scale']
     t = 1.0 - (args['timestep'] / 999.0)[:, None, None, None].clone()
-    
+
     alpha = 0.001 * sharpness * t
     eps_degraded = anisotropic.adaptive_anisotropic_filter(x=positive_eps, g=positive_x0)
     eps_degraded_weighted = eps_degraded * alpha + positive_eps * (1.0 - alpha)
 
     cond = eps_degraded_weighted * cfg_s + cfg_x0
-
     return adaptive_stylization(cond, uncond, cond_scale)
 
 
