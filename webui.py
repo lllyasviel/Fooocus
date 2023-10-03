68597580c1b164453b798ec19414e6bbe3da280c
lvmin <lyuminzhang@outlook.com>
2023-10-03 00:09:16 -0700

    i

--- webui.py
+++ webui.py
@@ -151,9 +151,12 @@ with shared.gradio_root:
                 with gr.Row():
                     model_refresh = gr.Button(label='Refresh', value='\U0001f504 Refresh All Files', variant='secondary', elem_classes='refresh_button')
             with gr.Tab(label='Advanced'):
-                sharpness = gr.Slider(label='Sampling Sharpness', minimum=0.0, maximum=30.0, step=0.001, value=2.0)
-                camera_distance = gr.Slider(label='Camera Distance', minimum=0.1, maximum=3.0, step=0.001, value=1.5)
-                stylize_influence = gr.Slider(label='Stylize Influence', minimum=10, maximum=500, step=1, value=100)
+                sharpness = gr.Slider(label='Sampling Sharpness', minimum=0.0, maximum=30.0, step=0.001, value=2.0,
+                                      info='Higher value means image and texture are sharper.')
+                camera_distance = gr.Slider(label='Camera Distance', minimum=0.1, maximum=3.0, step=0.001, value=1.5,
+                                      info='Higher value means camera is more far away and image contains more objects.')
+                stylize_influence = gr.Slider(label='Stylize Influence', minimum=10, maximum=500, step=1, value=100,
+                                      info='Higher value means style is cleaner, vivider, and more artistic.')
                 gr.HTML('<a href="https://github.com/lllyasviel/Fooocus/discussions/117">\U0001F4D4 Document</a>')
 
                 def model_refresh_clicked():
