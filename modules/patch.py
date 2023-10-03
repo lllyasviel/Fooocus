dfd49996af6bf876814c34519ea4be0230766349
lvmin <lyuminzhang@outlook.com>
2023-10-02 22:40:35 -0700

    i

--- modules/patch.py
+++ modules/patch.py
@@ -21,7 +21,7 @@ from comfy.ldm.modules.diffusionmodules.openaimodel import timestep_embedding, f
 
 
 sharpness = 2.0
-adm_scale = 1.0
+adm_scale = 1.25
 
 cfg_x0 = 0.0
 cfg_s = 1.0
@@ -183,10 +183,8 @@ def patched_model_function_wrapper(func, args):
     return func(x, t, **c)
 
 
-def sdxl_encode_adm_patched(self, **kwargs):
-    global negative_adm
-
-    clip_pooled = kwargs["pooled_output"]
+def encode_adm(self, **kwargs):
+    clip_pooled = comfy.model_base.sdxl_pooled(kwargs, self.noise_augmentor)
     width = kwargs.get("width", 768)
     height = kwargs.get("height", 768)
     crop_w = kwargs.get("crop_w", 0)
@@ -194,13 +192,12 @@ def sdxl_encode_adm_patched(self, **kwargs):
     target_width = kwargs.get("target_width", width)
     target_height = kwargs.get("target_height", height)
 
-    if negative_adm:
-        if kwargs.get("prompt_type", "") == "negative":
-            width *= 0.8
-            height *= 0.8
-        elif kwargs.get("prompt_type", "") == "positive":
-            width *= 1.5
-            height *= 1.5
+    if kwargs.get("prompt_type", "") == "negative":
+        width = float(width) * adm_scale
+        height = float(height) * adm_scale
+    elif kwargs.get("prompt_type", "") == "positive":
+        width = float(width) / adm_scale
+        height = float(height) / adm_scale
 
     out = []
     out.append(self.embedder(torch.Tensor([height])))
@@ -209,7 +206,7 @@ def sdxl_encode_adm_patched(self, **kwargs):
     out.append(self.embedder(torch.Tensor([crop_w])))
     out.append(self.embedder(torch.Tensor([target_height])))
     out.append(self.embedder(torch.Tensor([target_width])))
-    flat = torch.flatten(torch.cat(out))[None, ]
+    flat = torch.flatten(torch.cat(out)).unsqueeze(dim=0).repeat(clip_pooled.shape[0], 1)
     return torch.cat((clip_pooled.to(flat.device), flat), dim=1)
 
 
