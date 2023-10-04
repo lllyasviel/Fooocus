aebfc56641547c1931d38474af62338530d265e3
lllyasviel <lyuminzhang@outlook.com>
2023-10-04 15:43:41 -0700

    fix (#543)
    * fix
    
    * fix
    
    * fix

--- webui.py
+++ webui.py
@@ -112,8 +112,8 @@ with shared.gradio_root:
 
         with gr.Column(scale=0.5, visible=False) as right_col:
             with gr.Tab(label='Setting'):
-                performance_selction = gr.Radio(label='Performance', choices=['Speed', 'Quality'], value='Speed')
-                aspect_ratios_selction = gr.Radio(label='Aspect Ratios', choices=list(aspect_ratios.keys()),
+                performance_selection = gr.Radio(label='Performance', choices=['Speed', 'Quality'], value='Speed')
+                aspect_ratios_selection = gr.Radio(label='Aspect Ratios', choices=list(aspect_ratios.keys()),
                                                   value='1152×896', info='width × height')
                 image_number = gr.Slider(label='Image Number', minimum=1, maximum=32, step=1, value=2)
                 negative_prompt = gr.Textbox(label='Negative Prompt', show_label=True, placeholder="Type prompt here.",
@@ -211,7 +211,7 @@ with shared.gradio_root:
         advanced_checkbox.change(lambda x: gr.update(visible=x), advanced_checkbox, right_col, queue=False)
         ctrls = [
             prompt, negative_prompt, style_selections,
-            performance_selction, aspect_ratios_selction, image_number, image_seed, sharpness, adm_scaler_positive, adm_scaler_negative, guidance_scale, adaptive_cfg, sampler_name, scheduler_name
+            performance_selection, aspect_ratios_selection, image_number, image_seed, sharpness, adm_scaler_positive, adm_scaler_negative, guidance_scale, adaptive_cfg, sampler_name, scheduler_name
         ]
         ctrls += overwrite_ctrls
         ctrls += [base_model, refiner_model] + lora_ctrls
