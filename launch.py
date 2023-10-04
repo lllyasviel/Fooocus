046c93c0f26ac1c35f5a6aa5a8c20a681e8a0963
lllyasviel <lyuminzhang@outlook.com>
2023-10-04 13:21:30 -0700

    see if pygit2 is fixed by changing a commit (#538)

--- launch.py
+++ launch.py
@@ -22,7 +22,7 @@ def prepare_environment():
     xformers_package = os.environ.get('XFORMERS_PACKAGE', 'xformers==0.0.20')
 
     comfy_repo = os.environ.get('COMFY_REPO', "https://github.com/comfyanonymous/ComfyUI")
-    comfy_commit_hash = os.environ.get('COMFY_COMMIT_HASH', "2ef459b1d4d627929c84d11e5e0cbe3ded9c9f48")
+    comfy_commit_hash = os.environ.get('COMFY_COMMIT_HASH', "9bfec2bdbf0b0d778087a9b32f79e57e2d15b913")
 
     print(f"Python {sys.version}")
     print(f"Fooocus version: {fooocus_version.version}")
