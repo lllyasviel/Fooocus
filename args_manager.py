778a3cf81ec2229d51ce0b2910bfcd27fd08f2e5
lllyasviel <lyuminzhang@outlook.com>
2023-10-10 02:21:52 -0700

    fix port (#621)

--- args_manager.py
+++ args_manager.py
@@ -9,4 +9,7 @@ comfy_cli.args = comfy_cli.parser.parse_args()
 comfy_cli.args.disable_cuda_malloc = True
 comfy_cli.args.auto_launch = True
 
+if getattr(comfy_cli.args, 'port', 8188) == 8188:
+    comfy_cli.args.port = None
+
 args = comfy_cli.args
