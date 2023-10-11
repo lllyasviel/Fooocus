9a1b8248b6d5142ddabdad1e0b15ee6299cc2076
lvmin <lyuminzhang@outlook.com>
2023-10-11 03:21:12 -0700

    sync

--- modules/default_pipeline.py
+++ modules/default_pipeline.py
@@ -387,6 +387,7 @@ def process_diffusion(positive_cond, negative_cond, steps, switch, width, height
             scheduler=scheduler_name,
             previewer_start=switch,
             previewer_end=steps,
+            noise_multiplier=1.2,
         )
 
         target_model = final_refiner_vae
