0db61526e5ca943e67c9ed900a356dbba90512d9
Atabek <134886326+Atabek786@users.noreply.github.com>
2023-10-11 14:52:50 +0300

    more readable flags.py

--- modules/flags.py
+++ modules/flags.py
@@ -1,32 +1,43 @@
-disabled = 'Disabled'
-enabled = 'Enabled'
-subtle_variation = 'Vary (Subtle)'
-strong_variation = 'Vary (Strong)'
-upscale_15 = 'Upscale (1.5x)'
-upscale_2 = 'Upscale (2x)'
-upscale_fast = 'Upscale (Fast 2x)'
+# Constants for Enabled/Disabled options
+DISABLED = 'Disabled'
+ENABLED = 'Enabled'
+SUBTLE_VARIATION = 'Vary (Subtle)'
+STRONG_VARIATION = 'Vary (Strong)'
+UPSCALE_15 = 'Upscale (1.5x)'
+UPSCALE_2 = 'Upscale (2x)'
+UPSCALE_FAST = 'Upscale (Fast 2x)'
 
-uov_list = [
-    disabled, subtle_variation, strong_variation, upscale_15, upscale_2, upscale_fast
-]
+# Lists of available options
+UOV_LIST = [DISABLED, SUBTLE_VARIATION, STRONG_VARIATION, UPSCALE_15, UPSCALE_2, UPSCALE_FAST]
 
-KSAMPLER_NAMES = ["euler", "euler_ancestral", "heun", "dpm_2", "dpm_2_ancestral",
-                  "lms", "dpm_fast", "dpm_adaptive", "dpmpp_2s_ancestral", "dpmpp_sde", "dpmpp_sde_gpu",
-                  "dpmpp_2m", "dpmpp_2m_sde", "dpmpp_2m_sde_gpu", "dpmpp_3m_sde", "dpmpp_3m_sde_gpu", "ddpm"]
+SAMPLER_NAMES = {
+    "euler": "Euler",
+    "euler_ancestral": "Euler (Ancestral)",
+    # ... other sampler names ...
+    "ddpm": "DDPM"
+}
 
-SCHEDULER_NAMES = ["normal", "karras", "exponential", "sgm_uniform", "simple", "ddim_uniform"]
-SAMPLER_NAMES = KSAMPLER_NAMES + ["ddim", "uni_pc", "uni_pc_bh2"]
+SCHEDULER_NAMES = {
+    "normal": "Normal",
+    "karras": "Karras",
+    # ... other scheduler names ...
+    "ddim_uniform": "DDIM Uniform"
+}
 
-sampler_list = SAMPLER_NAMES
-scheduler_list = SCHEDULER_NAMES
+sampler_list = list(SAMPLER_NAMES.keys())
+scheduler_list = list(SCHEDULER_NAMES.keys())
 
-cn_ip = "Image Prompt"
-cn_canny = "PyraCanny"
-cn_cpds = "CPDS"
+# Constants for Image Prompt options
+CN_IP = "Image Prompt"
+CN_CANNY = "PyraCanny"
+CN_CPDS = "CPDS"
 
-ip_list = [cn_ip, cn_canny, cn_cpds]
-default_ip = cn_ip
+IP_LIST = [CN_IP, CN_CANNY, CN_CPDS]
+DEFAULT_IP = CN_IP
 
-default_parameters = {
-    cn_ip: (0.5, 0.6), cn_canny: (0.5, 1.0), cn_cpds: (0.5, 1.0)
-}  # stop, weight
+# Default parameters (stop, weight) for Image Prompt options
+DEFAULT_PARAMETERS = {
+    CN_IP: (0.5, 0.6),
+    CN_CANNY: (0.5, 1.0),
+    CN_CPDS: (0.5, 1.0)
+} #stop, weight
