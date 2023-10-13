80ad010c7d3ca93e1877af552e48d3d3d8ed9ea5
lvmin <lyuminzhang@outlook.com>
2023-10-13 16:38:03 -0700

    i

--- modules/path.py
+++ modules/path.py
@@ -138,6 +138,14 @@ checkpoint_downloads = get_config_item_or_set_default(
     },
     validator=lambda x: isinstance(x, dict) and all(isinstance(k, str) and isinstance(v, str) for k, v in x.items())
 )
+lora_downloads = get_config_item_or_set_default(
+    key='lora_downloads',
+    default_value={
+        'sd_xl_offset_example-lora_1.0.safetensors':
+            'https://huggingface.co/stabilityai/stable-diffusion-xl-base-1.0/resolve/main/sd_xl_offset_example-lora_1.0.safetensors'
+    },
+    validator=lambda x: isinstance(x, dict) and all(isinstance(k, str) and isinstance(v, str) for k, v in x.items())
+)
 embeddings_downloads = get_config_item_or_set_default(
     key='embeddings_downloads',
     default_value={},
