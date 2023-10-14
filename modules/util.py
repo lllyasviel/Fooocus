9cb02a0b9216ac3d76a58a94e01989dbf08c89d3
Oleg Afanasev <oleg.afanasyev02@gmail.com>
2023-10-14 16:04:53 +0300

    decreasing cyclomatic complexity

--- modules/util.py
+++ modules/util.py
@@ -8,7 +8,7 @@ from PIL import Image
 
 def image_is_generated_in_current_ui(image, ui_width, ui_height):
     H, W, C = image.shape
-    
+
     if H < ui_height:
         return False
 
@@ -34,6 +34,45 @@ def resample_image(im, width, height):
     return np.array(im)
 
 
+def resize_with_default_mode(im, width, height, resize_mode):
+    if resize_mode != 1:
+        return None
+    ratio = width / height
+    src_ratio = im.width / im.height
+    src_w = width if ratio > src_ratio else im.width * height // im.height
+    src_h = height if ratio <= src_ratio else im.height * width // im.width
+    resized = im.resize((src_w, src_h), resample=LANCZOS)
+    res = Image.new("RGB", (width, height))
+    res.paste(resized, box=(width // 2 - src_w // 2, height // 2 - src_h // 2))
+    return res
+
+
+def resize_with_last_mode(im, width, height):
+    ratio = width / height
+    src_ratio = im.width / im.height
+
+    src_w = width if ratio < src_ratio else im.width * height // im.height
+    src_h = height if ratio >= src_ratio else im.height * width // im.width
+
+    resized = im.resize((src_w, src_h), resample=LANCZOS)
+    res = Image.new("RGB", (width, height))
+    res.paste(resized, box=(width // 2 - src_w // 2, height // 2 - src_h // 2))
+
+    if ratio < src_ratio:
+        fill_height = height // 2 - src_h // 2
+        if fill_height > 0:
+            res.paste(resized.resize((width, fill_height), box=(0, 0, width, 0)), box=(0, 0))
+            res.paste(resized.resize((width, fill_height), box=(0, resized.height, width, resized.height)),
+                      box=(0, fill_height + src_h))
+    elif ratio > src_ratio:
+        fill_width = width // 2 - src_w // 2
+        if fill_width > 0:
+            res.paste(resized.resize((fill_width, height), box=(0, 0, 0, height)), box=(0, 0))
+            res.paste(resized.resize((fill_width, height), box=(resized.width, 0, resized.width, height)),
+                      box=(fill_width + src_w, 0))
+    return res
+
+
 def resize_image(im, width, height, resize_mode=1):
     """
     Resizes an image with the specified resize_mode, width, and height.
@@ -47,49 +86,13 @@ def resize_image(im, width, height, resize_mode=1):
         width: The width to resize the image to.
         height: The height to resize the image to.
     """
-
     im = Image.fromarray(im)
-
-    def resize(im, w, h):
-        return im.resize((w, h), resample=LANCZOS)
-
-    if resize_mode == 0:
-        res = resize(im, width, height)
-
-    elif resize_mode == 1:
-        ratio = width / height
-        src_ratio = im.width / im.height
-
-        src_w = width if ratio > src_ratio else im.width * height // im.height
-        src_h = height if ratio <= src_ratio else im.height * width // im.width
-
-        resized = resize(im, src_w, src_h)
-        res = Image.new("RGB", (width, height))
-        res.paste(resized, box=(width // 2 - src_w // 2, height // 2 - src_h // 2))
-
-    else:
-        ratio = width / height
-        src_ratio = im.width / im.height
-
-        src_w = width if ratio < src_ratio else im.width * height // im.height
-        src_h = height if ratio >= src_ratio else im.height * width // im.width
-
-        resized = resize(im, src_w, src_h)
-        res = Image.new("RGB", (width, height))
-        res.paste(resized, box=(width // 2 - src_w // 2, height // 2 - src_h // 2))
-
-        if ratio < src_ratio:
-            fill_height = height // 2 - src_h // 2
-            if fill_height > 0:
-                res.paste(resized.resize((width, fill_height), box=(0, 0, width, 0)), box=(0, 0))
-                res.paste(resized.resize((width, fill_height), box=(0, resized.height, width, resized.height)), box=(0, fill_height + src_h))
-        elif ratio > src_ratio:
-            fill_width = width // 2 - src_w // 2
-            if fill_width > 0:
-                res.paste(resized.resize((fill_width, height), box=(0, 0, 0, height)), box=(0, 0))
-                res.paste(resized.resize((fill_width, height), box=(resized.width, 0, resized.width, height)), box=(fill_width + src_w, 0))
-
-    return np.array(res)
+    result = im.resize((width, height), resample=LANCZOS)
+    if resize_mode != 0:
+        result = resize_with_default_mode(im, width, height, resize_mode)
+        if result is None:
+            result = resize_with_last_mode(im, width, height)
+    return np.array(result)
 
 
 def make_sure_that_image_is_not_too_large(x):
