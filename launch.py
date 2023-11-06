933da40735683f7d9e49d8fd6ca7ccb5033ef925
lllyasviel <lyuminzhang@outlook.com>
2023-11-06 00:01:04 -0800

    also launch from launch.py

--- launch.py
+++ launch.py
@@ -1,6 +1,16 @@
-from python_hijack import *
-
+import os
 import sys
+
+
+root = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(root)
+os.chdir(root)
+backend_path = os.path.join(root, 'backend', 'headless')
+if backend_path not in sys.path:
+    sys.path.append(backend_path)
+os.environ["PYTORCH_ENABLE_MPS_FALLBACK"] = "1"
+
+
 import platform
 import fooocus_version
 
