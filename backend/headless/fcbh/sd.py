38b01230f2d86da6e39af25b5c3a7232d9ffe819
lllyasviel <lyuminzhang@outlook.com>
2023-11-11 23:46:55 -0800

    Fooocus GitHub Bot Commit
    This commit is generated by a GitHub bot of Fooocus

--- backend/headless/fcbh/sd.py
+++ backend/headless/fcbh/sd.py
@@ -35,7 +35,7 @@ def load_model_weights(model, sd):
             w = sd.pop(x)
             del w
     if len(m) > 0:
-        print("Visited extra keys:", m)
+        print("extra keys", m)
     return model
 
 def load_clip_weights(model, sd):
