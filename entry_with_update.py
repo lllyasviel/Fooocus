093e2b40e7dc3df2a31a192113235cb2d46e8a1d
Ana Els <77629566+anae-git@users.noreply.github.com>
2023-12-06 13:22:28 -0800

    Update entry_with_update.py
    Fix grammar here

--- entry_with_update.py
+++ entry_with_update.py
@@ -37,7 +37,7 @@ try:
         repo.reset(local_branch.target, pygit2.GIT_RESET_HARD)
         print("Fast-forward merge")
     elif merge_result & pygit2.GIT_MERGE_ANALYSIS_NORMAL:
-        print("Update failed - Did you modified any file?")
+        print("Update failed - Have you modified any files?")
 except Exception as e:
     print('Update failed.')
     print(str(e))
