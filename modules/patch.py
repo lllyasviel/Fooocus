9c3fb90df3a38b71664c03d116bea3ac1d8f341a
lllyasviel <lyuminzhang@outlook.com>
2023-12-12 14:14:30 -0800

    try fix cpu tree

--- modules/patch.py
+++ modules/patch.py
@@ -181,6 +181,9 @@ class BrownianTreeNoiseSamplerPatched:
 
     @staticmethod
     def global_init(x, sigma_min, sigma_max, seed=None, transform=lambda x: x, cpu=False):
+        if ldm_patched.modules.model_management.directml_enabled:
+            cpu = True
+
         t0, t1 = transform(torch.as_tensor(sigma_min)), transform(torch.as_tensor(sigma_max))
 
         BrownianTreeNoiseSamplerPatched.transform = transform
