7d0f17ffb17aef79c40fc77417eb825bd2f418d5
Huy Nguyen <111590292+peterng1618@users.noreply.github.com>
2023-12-18 09:44:05 +0700

    Update the log file html layout
    - Dark theme
    - Click on thumbnail to open the full-size image
    - Metadata presented in a table layout

--- modules/private_logger.py
+++ modules/private_logger.py
@@ -25,29 +25,49 @@ def log(img, dic, single_line_number=3):
     Image.fromarray(img).save(local_temp_filename)
     html_name = os.path.join(os.path.dirname(local_temp_filename), 'log.html')
 
+    css_styles = (
+        "<style>"
+        "body { background-color: #121212; color: #E0E0E0; } "
+        "a { color: #BB86FC; } "
+        ".metadata { border-collapse: collapse; width: 100%; } "
+        ".metadata .key { width: 15%; } "
+        ".metadata .value { width: 85%; font-weight: bold; } "
+        ".metadata th, .metadata td { border: 1px solid #4d4d4d; padding: 4px; } "
+        ".image-container img { height: auto; max-width: 512px; display: block; padding-right:10px; } "
+        ".image-container div { text-align: center; padding: 4px; } "
+        "</style>"
+    )
+
     existing_log = log_cache.get(html_name, None)
 
     if existing_log is None:
         if os.path.exists(html_name):
-            existing_log = open(html_name, encoding='utf-8').read()
+            existing_log = open(html_name, 'r', encoding='utf-8').read()
         else:
-            existing_log = f'<p>Fooocus Log {date_string} (private)</p>\n<p>All images do not contain any hidden data.</p>'
+            existing_log = f"<html><head>{css_styles}</head><body>"
+            existing_log += f'\n<p>Fooocus Log {date_string} (private)</p>\n<p>All images do not contain any hidden data.</p>'
 
     div_name = only_name.replace('.', '_')
-    item = f'<div id="{div_name}">\n'
+    item = f'<div id="{div_name}" class="image-container">\n'
     item += "<table><tr>"
-    item += f"<td><img src=\"{only_name}\" width=auto height=100% loading=lazy style=\"height:auto;max-width:512px\" onerror=\"document.getElementById('{div_name}').style.display = 'none';\"></img></p></td>"
-    item += f"<td style=\"padding-left:10px;\"><p>{only_name}</p>\n"
-    for i, (k, v) in enumerate(dic):
-        if i < single_line_number:
-            item += f"<p>{k}: <b>{v}</b></p>\n"
-        else:
-            if (i - single_line_number) % 2 == 0:
-                item += f"<p>{k}: <b>{v}</b>, "
-            else:
-                item += f"{k}: <b>{v}</b></p>\n"
+    item += f"<td><a href='{only_name}'><img src='{only_name}' onerror=\"this.closest('.image-container').style.display='none';\" loading='lazy'></img></a><div>{only_name}</div></td>"
+    item += "<td>"
+    item += "<table class='metadata'>"
+
+    if isinstance(dic, list):
+        for item_tuple in dic:
+            if len(item_tuple) == 2:
+                key, value = item_tuple
+                if key.startswith('LoRA [') and ']' in key:
+                    lora_name = key[key.find('[') + 1 : key.find(']')]
+                    rest_of_key = key[key.find(']') + 2:]
+                    item += f"<tr><td class='key'>LoRA</td><td class='value'>{lora_name}: {value}</td></tr>\n"
+                else:
+                    item += f"<tr><td class='key'>{key}</td><td class='value'>{value}</td></tr>\n"
+
+    item += "</table>"
     item += "</td>"
-    item += "</tr></table><hr></div>\n"
+    item += "</tr></table><hr></div>\n\n"
     existing_log = item + existing_log
 
     with open(html_name, 'w', encoding='utf-8') as f:
