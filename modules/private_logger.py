366b3d657eee934a093805cd02b5f6b6b20df68f
lllyasviel <lyuminzhang@outlook.com>
2023-12-17 20:09:46 -0800

    Update private_logger.py

--- modules/private_logger.py
+++ modules/private_logger.py
@@ -39,7 +39,7 @@ def log(img, dic):
         "</style>"
     )
 
-    begin_part = f"<html><head><title>Fooocus Log {date_string}</title>{css_styles}</head><body><p>Fooocus Log {date_string} (private)</p>\n<p>All images do not contain any hidden data.</p><!--fooocus-log-split-->\n\n"
+    begin_part = f"<html><head><title>Fooocus Log {date_string}</title>{css_styles}</head><body><p>Fooocus Log {date_string} (private)</p>\n<p>All images are clean, without any hidden data/meta, and safe to share with others.</p><!--fooocus-log-split-->\n\n"
     end_part = f'\n<!--fooocus-log-split--></body></html>'
 
     middle_part = log_cache.get(html_name, "")
