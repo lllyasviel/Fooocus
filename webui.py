1d04c9d02f11437f8c66ec14de111d78d9f9e3ee
xhoxye <129571231+xhoxye@users.noreply.github.com>
2024-01-29 19:23:24 +0800

    合并通配符顺序读取功能，Fooocus wildcards order read (#18)
    * 通配符增强，切换顺序读取
    
    通配符增强，通过勾选切换通配符读取方法，默认不勾选为随机读取一行，勾选后为按顺序读取，并使用相同的种子。
    
    * 代码来自刁璐璐
    
    * update
    
    * Update async_worker.py

--- webui.py
+++ webui.py
@@ -255,6 +255,8 @@ with shared.gradio_root:
                 seed_random.change(random_checked, inputs=[seed_random], outputs=[image_seed],
                                    queue=False, show_progress=False)
 
+                read_wildcard_in_order_checkbox = gr.Checkbox(label="Read wildcard in order with same seed", value=False)
+
                 if not args_manager.args.disable_image_log:
                     gr.HTML(f'<a href="file={get_current_html_path()}" target="_blank">\U0001F4DA History Log</a>')
 
@@ -521,7 +523,7 @@ with shared.gradio_root:
 
         ctrls = [
             prompt, negative_prompt, style_selections,
-            performance_selection, aspect_ratios_selection, image_number, image_seed, sharpness, guidance_scale
+            performance_selection, aspect_ratios_selection, image_number, image_seed, read_wildcard_in_order_checkbox, sharpness, guidance_scale
         ]
 
         ctrls += [base_model, refiner_model, refiner_switch] + lora_ctrls
