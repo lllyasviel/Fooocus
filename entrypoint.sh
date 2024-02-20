34d7617fcf4dcfa1fdd666d478e50fef428f2c4e
Shiro Hara <white@vx-xv.com>
2024-02-21 07:57:46 +0900

    Import files from models,outputs

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,15 @@ ORIGINALDIR=/content/app
 # Use predefined DATADIR if it is defined
 [[ x"${DATADIR}" == "x" ]] && DATADIR=/content/data
 
-# make persistent dir from original dir
+# Make persistent dir from original dir
 function mklink () {
 	mkdir -p $DATADIR/$1
 	ln -s $DATADIR/$1 $ORIGINALDIR
+}
+
+# Copy old files from import dir
+function import () {
+	(test -d /import/$1 && cd /import/$1 && cp -Rpn . $DATADIR/$1/)
 }
 
 cd $ORIGINALDIR
@@ -16,9 +21,13 @@ cd $ORIGINALDIR
 mklink models
 # Copy original files
 (cd $ORIGINALDIR/models.org && cp -Rpn . $ORIGINALDIR/models/)
+# Import old files
+import models
 
 # outputs
 mklink outputs
+# Import old files
+import outputs
 
 # Start application
 python launch.py $*
