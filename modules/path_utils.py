1362533eda090088256c1eb6a23e2a00d3520c83
cantor-set <cantor-set@no-email.net>
2024-03-03 20:47:17 -0500

    Fixed typo. Added TODO

--- modules/path_utils.py
+++ modules/path_utils.py
@@ -1,19 +1,20 @@
 
 import os
 
-def get_files_from_folder(folder_path, exensions=None, name_filter=None):
+#TODO: Use refactor to use glob instead
+def get_files_from_folder(folder_path, extensions=None, name_filter=None):
     if not os.path.isdir(folder_path):
         raise ValueError("Folder path is not a valid directory.")
 
     filenames = []
 
-    for root, dirs, files in os.walk(folder_path, topdown=False):
+    for root, _, files in os.walk(folder_path, topdown=False):
         relative_path = os.path.relpath(root, folder_path)
         if relative_path == ".":
             relative_path = ""
         for filename in sorted(files, key=lambda s: s.casefold()):
             _, file_extension = os.path.splitext(filename)
-            if (exensions is None or file_extension.lower() in exensions) and (name_filter is None or name_filter in _):
+            if (extensions is None or file_extension.lower() in extensions) and (name_filter is None or name_filter in _):
                 path = os.path.join(relative_path, filename)
                 filenames.append(path)
 
