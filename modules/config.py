ce393d08e450f39f89cb126ab936d7fcf032036e
Manuel Schmid <manuel.schmid@odt.net>
2024-03-10 23:44:22 +0100

    feat: allow to add disabled LoRAs in config
    add LoRA checkbox enable/disable handling to all necessary occurrences

--- modules/config.py
+++ modules/config.py
@@ -240,27 +240,32 @@ default_loras = get_config_item_or_set_default(
     key='default_loras',
     default_value=[
         [
+            True,
             "None",
             1.0
         ],
         [
+            True,
             "None",
             1.0
         ],
         [
+            True,
             "None",
             1.0
         ],
         [
+            True,
             "None",
             1.0
         ],
         [
+            True,
             "None",
             1.0
         ]
     ],
-    validator=lambda x: isinstance(x, list) and all(len(y) == 2 and isinstance(y[0], str) and isinstance(y[1], numbers.Number) for y in x)
+    validator=lambda x: isinstance(x, list) and all(len(y) == 3 and isinstance(y[0], bool) and isinstance(y[1], str) and isinstance(y[2], numbers.Number) for y in x)
 )
 default_max_lora_number = get_config_item_or_set_default(
     key='default_max_lora_number',
