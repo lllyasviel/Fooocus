f289174af7426cdb8c8065ca2a375609903cd9ba
cantor-set <cantor-set@no-email.net>
2024-03-21 22:46:59 -0400

    Added missing refernce function

--- modules/async_worker.py
+++ modules/async_worker.py
@@ -47,7 +47,7 @@ def worker():
     from modules.private_logger import log
     from extras.expansion import safe_str
     from modules.util import remove_empty_str, HWC3, resize_image, get_image_shape_ceil, set_image_shape_ceil, \
-        get_shape_ceil, resample_image, erode_or_dilate, ordinal_suffix, get_enabled_loras
+        get_shape_ceil, resample_image, erode_or_dilate, ordinal_suffix, get_enabled_loras, parse_lora_references_from_prompt
     from modules.upscaler import perform_upscale
     from modules.flags import Performance
     from modules.meta_parser import get_metadata_parser, MetadataScheme
