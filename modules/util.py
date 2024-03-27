6fed0961234a2068fcca37f5f0d511b1f85a372c
cantor-set <cantor-set@no-email.net>
2024-03-27 12:36:05 -0400

    Removed function from util.py... again...

--- modules/util.py
+++ modules/util.py
@@ -402,22 +402,3 @@ def parse_lora_references_from_prompt(prompt: str, loras: List[Tuple[AnyStr, flo
             updated_loras.append(lora)
 
     return updated_loras[:loras_limit]
-
-def get_files_from_folder(folder_path, extensions=None, name_filter=None):
-    if not os.path.isdir(folder_path):
-        raise ValueError("Folder path is not a valid directory.")
-
-    filenames = []
-
-    for root, dirs, files in os.walk(folder_path, topdown=False):
-        relative_path = os.path.relpath(root, folder_path)
-        if relative_path == ".":
-            relative_path = ""
-        for filename in sorted(files, key=lambda s: s.casefold()):
-            _, file_extension = os.path.splitext(filename)
-            if (extensions is None or file_extension.lower() in extensions) and (name_filter is None or name_filter in _):
-                path = os.path.join(relative_path, filename)
-                filenames.append(path)
-
-    return filenames
-
