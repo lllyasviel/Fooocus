450f11de658416b0b20db37b6ad85d6d24e623b8
Jodh Singh <jetjodh@gmail.com>
2024-04-03 11:28:16 -0400

    updated with new download link

--- launch.py
+++ launch.py
@@ -62,8 +62,8 @@ def prepare_environment():
 vae_approx_filenames = [
     ('xlvaeapp.pth', 'https://huggingface.co/lllyasviel/misc/resolve/main/xlvaeapp.pth'),
     ('vaeapp_sd15.pth', 'https://huggingface.co/lllyasviel/misc/resolve/main/vaeapp_sd15.pt'),
-    ('xl-to-v1_interposer-v3.1.safetensors',
-     'https://huggingface.co/lllyasviel/misc/resolve/main/xl-to-v1_interposer-v3.1.safetensors')
+    ('xl-to-v1_interposer-v4.0.safetensors',
+     'https://huggingface.co/city96/SD-Latent-Interposer/resolve/main/v4.0/xl-to-v1_interposer-v4.0.safetensors')
 ]
 
 
