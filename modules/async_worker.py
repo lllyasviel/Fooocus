5ada070d88f0527568d2c8c3ac77d3e12d77997b
delta_lt_0 <chenxinlong2009@gmail.com>
2024-04-06 21:25:19 +0800

    feat: support download of huggingface files from a mirror website (#2637)
    * fix: load image number from preset (#2611)
    
    * fix: add default_image_number to preset handling
    
    * fix: use minimum image number of preset and config to prevent UI overflow
    
    * fix: use correct base dimensions for outpaint mask padding (#2612)
    
    * fix: add Civitai compatibility for LoRAs in a1111 metadata scheme by switching schema (#2615)
    
    * feat: update sha256 generation functions
    
    https://github.com/lllyasviel/stable-diffusion-webui-forge/blob/29be1da7cf2b5dccfc70fbdd33eb35c56a31ffb7/modules/hashes.py
    
    * feat: add compatibility for LoRAs in a1111 metadata scheme
    
    * feat: add backwards compatibility
    
    * refactor: extract remove_special_loras
    
    * fix: correctly apply LoRA weight for legacy schema
    
    * docs: bump version number to 2.3.1, add changelog (#2616)
    
    * feat:support download huggingface files from a  mirror site
    
    ---------
    
    Co-authored-by: Manuel Schmid <9307310+mashb1t@users.noreply.github.com>

--- modules/async_worker.py
+++ modules/async_worker.py
@@ -614,12 +614,12 @@ def worker():
 
                 H, W, C = inpaint_image.shape
                 if 'left' in outpaint_selections:
-                    inpaint_image = np.pad(inpaint_image, [[0, 0], [int(H * 0.3), 0], [0, 0]], mode='edge')
-                    inpaint_mask = np.pad(inpaint_mask, [[0, 0], [int(H * 0.3), 0]], mode='constant',
+                    inpaint_image = np.pad(inpaint_image, [[0, 0], [int(W * 0.3), 0], [0, 0]], mode='edge')
+                    inpaint_mask = np.pad(inpaint_mask, [[0, 0], [int(W * 0.3), 0]], mode='constant',
                                           constant_values=255)
                 if 'right' in outpaint_selections:
-                    inpaint_image = np.pad(inpaint_image, [[0, 0], [0, int(H * 0.3)], [0, 0]], mode='edge')
-                    inpaint_mask = np.pad(inpaint_mask, [[0, 0], [0, int(H * 0.3)]], mode='constant',
+                    inpaint_image = np.pad(inpaint_image, [[0, 0], [0, int(W * 0.3)], [0, 0]], mode='edge')
+                    inpaint_mask = np.pad(inpaint_mask, [[0, 0], [0, int(W * 0.3)]], mode='constant',
                                           constant_values=255)
 
                 inpaint_image = np.ascontiguousarray(inpaint_image.copy())
