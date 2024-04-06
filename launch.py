d2fbc480911bc105265b60f8957c95cf4dad3d76
Manuel Schmid <9307310+mashb1t@users.noreply.github.com>
2024-04-06 14:29:15 +0200

    fix: use correct URL

--- launch.py
+++ launch.py
@@ -63,7 +63,7 @@ vae_approx_filenames = [
     ('xlvaeapp.pth', 'https://huggingface.co/lllyasviel/misc/resolve/main/xlvaeapp.pth'),
     ('vaeapp_sd15.pth', 'https://huggingface.co/lllyasviel/misc/resolve/main/vaeapp_sd15.pt'),
     ('xl-to-v1_interposer-v4.0.safetensors',
-     'https://huggingface.co/mashb1t/misc/resolve/main/v4.0/xl-to-v1_interposer-v4.0.safetensors')
+     'https://huggingface.co/mashb1t/misc/resolve/main/xl-to-v1_interposer-v4.0.safetensors')
 ]
 
 
