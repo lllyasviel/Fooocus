7c1a101c0f4bffc7b596cd7c0cb258a6fb843145
Manuel Schmid <9307310+mashb1t@users.noreply.github.com>
2024-06-16 18:53:20 +0200

    hotfix: add missing method in performance enum (#3154)

--- modules/flags.py
+++ modules/flags.py
@@ -156,6 +156,10 @@ class Performance(Enum):
         return list(map(lambda c: c.value, cls))
 
     @classmethod
+    def values(cls) -> list:
+        return list(map(lambda c: c.value, cls))
+
+    @classmethod
     def by_steps(cls, steps: int | str):
         return cls[Steps(int(steps)).name]
 
