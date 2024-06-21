ec533f0c427afa89adcb1843065f6aefba72bf3d
Manuel Schmid <dev@mash1t.de>
2024-06-22 00:07:32 +0200

    feat: rename arg --enable-describe-uov-image to --enable-auto-describe-image, add handling for enhance input image

--- webui.py
+++ webui.py
@@ -1001,15 +1001,18 @@ with shared.gradio_root:
         desc_btn.click(trigger_describe, inputs=[desc_method, desc_input_image],
                        outputs=[prompt, style_selections], show_progress=True, queue=True)
 
-        if args_manager.args.enable_describe_uov_image:
-            def trigger_uov_describe(mode, img, prompt):
+        if args_manager.args.enable_auto_describe_image:
+            def trigger_auto_describe(mode, img, prompt):
                 # keep prompt if not empty
                 if prompt == '':
                     return trigger_describe(mode, img)
                 return gr.update(), gr.update()
 
-            uov_input_image.upload(trigger_uov_describe, inputs=[desc_method, uov_input_image, prompt],
-                           outputs=[prompt, style_selections], show_progress=True, queue=True)
+            uov_input_image.upload(trigger_auto_describe, inputs=[desc_method, uov_input_image, prompt],
+                                   outputs=[prompt, style_selections], show_progress=True, queue=True)
+
+            enhance_input_image.upload(trigger_auto_describe, inputs=[desc_method, enhance_input_image, prompt],
+                                   outputs=[prompt, style_selections], show_progress=True, queue=True)
 
 def dump_default_english_config():
     from modules.localization import dump_english_config
