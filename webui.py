b06404f640566e736cb030ceadd82ee7ec242001
andysama79 <arnab.dey@plaksha.edu.in>
2024-09-22 01:41:43 +0530

    share=True in launch() by default

--- webui.py
+++ webui.py
@@ -1121,7 +1121,7 @@ shared.gradio_root.launch(
     inbrowser=args_manager.args.in_browser,
     server_name=args_manager.args.listen,
     server_port=args_manager.args.port,
-    share=args_manager.args.share,
+    share=True,
     auth=check_auth if (args_manager.args.share or args_manager.args.listen) and auth_enabled else None,
     allowed_paths=[modules.config.path_outputs],
     blocked_paths=[constants.AUTH_FILENAME]
