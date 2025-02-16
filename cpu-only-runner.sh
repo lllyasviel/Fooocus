9886898bbcc4e0d79e529f7b6459735db06c49f4
Rustam B. <rustam.bogubaev@gmail.com>
2025-02-15 20:19:56 -0500

    Was able to run cpu-only-fooocus using docker compose

--- cpu-only-runner.sh
+++ cpu-only-runner.sh
@@ -3,12 +3,15 @@
 CPU_ONLY_FOOCUS_HOME="~/.cpu-only-fooocus"
 CPU_ONLY_FOOCUS_TMP="$CPU_ONLY_FOOCUS_HOME/tmp"
 CPU_ONLY_FOOCUS_MODULES="$CPU_ONLY_FOOCUS_HOME/modules"
+CPU_ONLY_FOOCUS_OUTPUTS="$CPU_ONLY_FOOCUS_HOME/outputs"
 
 mkdir -pv "$CPU_ONLY_FOOCUS_TMP"
 mkdir -pv "$CPU_ONLY_FOOCUS_MODULES"
+mkdir -pv "$CPU_ONLY_FOOCUS_OUTPUTS"
 
-docker build -t cpu-only-fooocus -f cpu-only-Dockerfile .
-docker run -it --rm -p 7865:7865 cpu-only-fooocus
+docker-compose -f cpu-only-docker-compose.yml up -d
 
+#docker build -t cpu-only-fooocus -f cpu-only-Dockerfile .
+#docker run -it --rm -p 7865:7865 cpu-only-fooocus
 #docker run -it --rm -p 7865:7865 -v "$CPU_ONLY_FOOCUS_TMP":/tmp/fooocus -v "$CPU_ONLY_FOOCUS_MODULES":/app/models cpu-only-fooocus
 
